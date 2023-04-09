e03e73ead990318fc14c30152c3a6d341d2729a7
nicolas ledesma <nico-ledesma98@hotmail.es>
2023-04-08 22:54:10 -0300

    github

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,5 +1,6 @@
 import local from 'passport-local' 
 import passport from 'passport'
+import GitHubStrategy from 'passport-github2'
 import { userMongoDB } from '../controllers/user.controller.js'
 import { createHash,validatePassword} from '../utils/bcrypt.js'
 ///passport se va a trabajar como un middleware
@@ -28,18 +29,9 @@ const initializePassport = () =>{
                 return done (error)
             }
         }))
-        ///inicializar la session del user
+       
         
-        passport.serializeUser((user,done) =>{
-            done(null, user._id)
-        })
-        // eliminar la session del user
-        passport.deserializeUser(async(id,done)=>{
-            const user = userMongoDB.getElementById(id)
-            done(null,user)
-        })
-        
-        passport.use('login',new LocalStrategy({usernameField : email}, async ( username, password,done) =>{
+        passport.use('login',new LocalStrategy({usernameField : 'email'}, async ( username, password,done) =>{
             try {
                 const user = await userMongoDB.getElementByEmail(username)
                 if(!user){//usuario no encontrado
@@ -53,6 +45,41 @@ const initializePassport = () =>{
                 return done(error)
             }
         }))
+
+        passport.use('github',new GitHubStrategy({
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: process.env.CALLBACK_URL,
+        },async (accessToken, refreshToken,profile,done) =>{
+            try{
+                console.log(accessToken)
+                const user = await userMongoDB.getElementByEmail(profile._json.email)
+                if(user){//si existe user en la bdd
+                    done(null,user)
+                }else{
+                    const userCreated = await userMongoDB.addElements([{
+                        first_name: profile._json.name,
+                        last_name:' ',//porque github no posee nombre y apellido
+                        email: profile._json.email,
+                        age: 25,//github no define la edad
+                        password:' ',//no puedo asignar una contraseña porque github ya me ofrece una
+                    }])
+                    done(null,userCreated)
+                }
+            }catch(error){
+                return done(error)
+            }
+        }))
+         ///inicializar la session del user
+        
+         passport.serializeUser((user,done) =>{
+            done(null, user._id)
+        })
+        // eliminar la session del user
+        passport.deserializeUser(async(id,done)=>{
+            const user = userMongoDB.getElementById(id)
+            done(null,user)
+        })
 }
 
 export default initializePassport
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ import routerProd from "./productRoutes.js";
 import routerCart from "./cartRoutes.js";
 import routerSession from "./sessionRoutes.js";
 import routerUser from "./userRoutes.js";
+import routerGithub from "./github.js";
 
 const router = Router()
 
@@ -10,5 +11,5 @@ router.use('/products',routerProd)
 router.use('/user',routerUser) 
 router.use('/api/cart',routerCart)
 router.use('/api/session',routerSession)
-
+router.use('/session',routerGithub)
 export default router
